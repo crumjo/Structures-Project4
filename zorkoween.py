ce53ac2c870531bada4fa6969c1e8d210f9ced68
Patton M Finley <finleyp@mail.gvsu.edu>
2017-11-08 11:31:51 -0500

    added some classes observable may not be right

--- zorkoween.py
+++ zorkoween.py
@@ -1,1 +1,30 @@
 #do stuff
+
+class Monster(observable):
+
+    
+class House(Observer):
+    def update(self):
+        print Observer + " added"
+
+class Person(Monster):
+
+class Observable(object):
+
+    def __init__(self):
+        self.observers = [];
+
+    def add_observer(self, observer):
+        if not observer in self.observers:
+            self.observers.append(observer);
+
+    def remove_observe(self, observer):
+        if observer in self.observers:
+            self.observers.remove(observer)
+
+    def remove_all_observers(self):
+        self.observers = []
+
+    def getMonsters(self):
+        return self.observers
+
